fc33ca8c3c8fabdad7e03460a86f6ff359eb3afb
tanaivcaci <tanaicc@gmail.com>
2021-08-26 03:21:54 -0300

    carga de dato universolector

--- C8S - Playground - Salud - UniversoLector/UniversoLector.sql
+++ C8S - Playground - Salud - UniversoLector/UniversoLector.sql
@@ -1,5 +1,5 @@
-#CREATE SCHEMA UniversoLector; 
-#USE UniversoLector; 
+CREATE SCHEMA UniversoLector; 
+USE UniversoLector; 
 
 CREATE TABLE UniversoLector.Socio (
     codigo INT NOT NULL, 
@@ -79,3 +79,37 @@ CREATE TABLE PrestamoxVolumen (
         FOREIGN KEY (codigo_volumen)
         REFERENCES UniversoLector.Volumen (codigo)
 );
+
+INSERT INTO `UniversoLector`.`Autor` (`codigo`, `apellido`, `nombres`) 
+VALUES ('1', 'Rowling', 'J. K.');
+
+INSERT INTO `UniversoLector`.`Editorial` (`codigo`, `razon_social`, `telefono`) VALUES ('1', 'Bloomsbury Publishing', '54911564874');
+INSERT INTO `UniversoLector`.`Editorial` (`codigo`, `razon_social`, `telefono`) VALUES ('2', 'Scholastic', '223483646');
+INSERT INTO `UniversoLector`.`Editorial` VALUES ('3', 'Pottermore Limited', '5694839582');
+INSERT INTO `UniversoLector`.`Editorial` VALUES ('4', 'Editorial Salamandra', '0112392343');
+
+INSERT INTO `UniversoLector`.`Libro` 
+(`codigo`, `isbn`, `titulo`, `anio_escritura`, `anio_edicion`, `codigo_autor`, `codigo_editorial`) 
+VALUES ('1', '9781907545009', 'Harry Potter y la piedra filosofal', '1997-01-01', '1997-01-01', '1', '4');
+INSERT INTO `UniversoLector`.`Libro` 
+(`codigo`, `isbn`, `titulo`, `anio_escritura`, `anio_edicion`, `codigo_autor`, `codigo_editorial`) 
+VALUES ('2', '9789878000114', 'Harry Potter Y La Camara Secreta ', '2020-01-01', '2020-01-01', '1', '4');
+
+INSERT INTO `UniversoLector`.`Socio` (`codigo`, `dni`, `apellido`, `nombres`, `direccion`, `localidad`) VALUES ('1', '3000000', 'PATRICIA', 'JOHNSON', '28 MySQL Boulevard', 'QLD');
+INSERT INTO `UniversoLector`.`Socio` (`codigo`, `dni`, `apellido`, `nombres`, `direccion`, `localidad`) VALUES ('2', '2988800', 'LINDA', 'WILLIAMS', '23 Workhaven Lane', 'Alberta');
+INSERT INTO `UniversoLector`.`Socio` (`codigo`, `dni`, `apellido`, `nombres`, `direccion`, `localidad`) VALUES ('3', '2500000', 'BARBARA', 'JONES', '1411 Lillydale Drive', 'QLD');
+INSERT INTO `UniversoLector`.`Socio` (`codigo`, `dni`, `apellido`, `nombres`, `direccion`, `localidad`) VALUES ('4', '32980002', 'LOIS', 'BUTLER', '1688 Okara Way', 'Nothwest Border Prov');
+INSERT INTO `UniversoLector`.`Socio` (`codigo`, `dni`, `apellido`, `nombres`, `direccion`, `localidad`) VALUES ('5', '2313909', 'ROBIN', 'HAYES', '262 A Corua (La Corua) Parkway', 'Dhaka');
+
+INSERT INTO `UniversoLector`.`Volumen` (`codigo`, `deteriorado`, `codigo_libro`) VALUES ('1', '0', '1');
+INSERT INTO `UniversoLector`.`Volumen` (`codigo`, `deteriorado`, `codigo_libro`) VALUES ('2', '0', '1');
+INSERT INTO `UniversoLector`.`Volumen` (`codigo`, `deteriorado`, `codigo_libro`) VALUES ('3', '0', '2');
+
+INSERT INTO `UniversoLector`.`Prestamo` (`codigo`, `fecha`, `fecha_devolucion`, `fecha_tope`, `codigo_socio`) VALUES ('2', '2020-01-07', '2020-01-15', '2020/01/14', '1');
+INSERT INTO `UniversoLector`.`Prestamo` (`codigo`, `fecha`, `fecha_devolucion`, `fecha_tope`, `codigo_socio`) VALUES ('3', '2020-03-04', '2020-03-08', '2020-11-03', '2');
+
+INSERT INTO `UniversoLector`.`PrestamoxVolumen` (`codigo`, `codigo_prestamo`, `codigo_volumen`) VALUES ('1', '1', '1');
+INSERT INTO `UniversoLector`.`PrestamoxVolumen` (`codigo`, `codigo_prestamo`, `codigo_volumen`) VALUES ('2', '2', '2');
+INSERT INTO `UniversoLector`.`PrestamoxVolumen` (`codigo`, `codigo_prestamo`, `codigo_volumen`) VALUES ('3', '3', '1');
+INSERT INTO `UniversoLector`.`PrestamoxVolumen` (`codigo`, `codigo_prestamo`, `codigo_volumen`) VALUES ('4', '3', '3');
+
