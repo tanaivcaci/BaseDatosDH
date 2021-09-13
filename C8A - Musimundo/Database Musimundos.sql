de2eb9f0ef47d618c5064fe6acdd1db73823b9e5
tanaicc <tanaicc@gmail.com>
2021-09-13 02:09:54 -0300

    C15S Musimundo al dia

--- C8A - Musimundo/Database Musimundos.sql
+++ C8A - Musimundo/Database Musimundos.sql
@@ -407,9 +407,7 @@ AND compositor = 'Willie Dixon';
 
 
 #C15S MESA
-
 # Consultas SELECT y GROUP BY
-
 # Realizar los siguientes informes:
 
 # 1. Listar las canciones cuya duración sea mayor a 2 minutos.
@@ -420,21 +418,74 @@ WHERE milisegundos > 120000;
 # 2. Listar las canciones cuyo nombre comience con una vocal.
 select * from canciones where nombre like ("a%") or nombre like ("e%") or nombre like ("i%") or nombre like ("o%") or nombre like ("u%"); 
 
-# 3. Listar las canciones ordenadas por compositor en forma descendente.
-# Luego, por nombre en forma ascendente. Incluir únicamente aquellas
-# canciones que tengan compositor.
+# 3. Listar las canciones ordenadas por compositor en forma descendente. Luego, por nombre en forma ascendente. Incluir únicamente aquellas canciones que tengan compositor.
+
+SELECT * FROM canciones
+WHERE compositor != ''
+ORDER BY compositor DESC, nombre DESC; 
+
 # 4. a) Listar la cantidad de canciones de cada compositor.
-# b) Modificar la consulta para incluir únicamente los compositores que
-# tengan más de 10 canciones.
-# 5. a) Listar el total facturado agrupado por ciudad.
-# b) Modificar el listado del punto (a) mostrando únicamente las ciudades
-# de Canadá.
-# c) Modificar el listado del punto (a) mostrando únicamente las ciudades
-# con una facturación mayor a 38.
-# d) Modificar el listado del punto (a) agrupando la facturación por país, y
-# luego por ciudad.
+
+SELECT compositor, COUNT(compositor) as cant
+FROM canciones
+WHERE compositor != ''
+GROUP BY compositor
+ORDER By cant DESC; 
+
+# b) Modificar la consulta para incluir únicamente los compositores que tengan más de 10 canciones.
+
+SELECT compositor, COUNT(compositor) as cant
+FROM canciones
+WHERE compositor != ''
+GROUP BY compositor
+HAVING cant > 10
+ORDER By cant DESC; 
+
+# 5. 
+# a) Listar el total facturado agrupado por ciudad.
+
+SELECT ciudad_de_facturacion, SUM(total) FROM facturas
+GROUP BY ciudad_de_facturacion; 
+
+
+# b) Modificar el listado del punto (a) mostrando únicamente las ciudades de Canadá.
+Select SUM(total) from facturas
+where ciudad_de_facturacion = 'Edmonton';
+
+SELECT ciudad_de_facturacion, SUM(total) 
+FROM facturas
+WHERE pais_de_facturacion = 'Canada'
+GROUP BY ciudad_de_facturacion; 
+
+# c) Modificar el listado del punto (a) mostrando únicamente las ciudades con una facturación mayor a 38.
+
+SELECT ciudad_de_facturacion, SUM(total) as facturacion
+FROM facturas
+GROUP BY ciudad_de_facturacion
+HAVING facturacion > 38; 
+
+
+# d) Modificar el listado del punto (a) agrupando la facturación por país, y luego por ciudad.
+
+SELECT pais_de_facturacion, ciudad_de_facturacion, SUM(total) FROM facturas
+GROUP BY pais_de_facturacion, ciudad_de_facturacion
+ORDER BY pais_de_facturacion; 
+
+
 # 6. a) Listar la duración mínima, máxima y promedio de las canciones.
+
+SELECT * FROM canciones; 
+SELECT MIN(milisegundos) as MinDuracion, MAX(milisegundos) as MaxDuracion, AVG(milisegundos) as PromedioDuracion
+FROM canciones;
+
 # b) Modificar el punto (a) mostrando la información agrupada por género.
 
+SELECT generos.nombre,
+MIN(milisegundos) as MinDuracion, 
+MAX(milisegundos) as MaxDuracion, 
+AVG(milisegundos) as PromedioDuracion
+FROM canciones
+INNER JOIN generos ON canciones.id_genero = generos.id
+GROUP BY id_genero;
 
 
