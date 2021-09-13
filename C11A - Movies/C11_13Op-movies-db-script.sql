66f081b34d0f6a64173aa833682040c46b896e3f
tanaivcaci <tanaicc@gmail.com>
2021-09-13 13:04:14 -0300

    PC C11 Y 14

--- C11A - Movies/C11_13Op-movies-db-script.sql
+++ C11A - Movies/C11_13Op-movies-db-script.sql
@@ -154,3 +154,17 @@ SELECT genre_id, COUNT(genre_id)
 FROM movies
 GROUP BY genre_id
 HAVING SUM(awards) > 5; 
+
+
+#C14A - OPCIONAL - DER EN CARPETA C14
+# Join
+# 1. Utilizando la base de datos de movies, queremos conocer, por un lado, los títulos y el nombre del género de todas las series de la base de datos.
+
+
+
+# 2. Por otro, necesitamos listar los títulos de los episodios junto con el nombre y apellido de los actores que trabajan en cada uno de ellos.
+
+# 3. Para nuestro próximo desafío, necesitamos obtener a todos los actores o actrices (mostrar nombre y apellido) que han trabajado en cualquier película de la saga de La Guerra de las galaxias.
+
+# 4. Crear un listado a partir de la tabla de películas, mostrar un reporte de la cantidad de películas por nombre de género.
+
--- C11S - EMarket/C11S QueriesMesa6MARKET.sql
+++ C11S - EMarket/C11S QueriesMesa6MARKET.sql
@@ -155,3 +155,71 @@ WHERE Apellido LIKE 'Buchanan';
 
 SELECT * FROM Facturas
 WHERE EmpleadoID = 5;
+
+
+#C14S - EMARKET
+# Join
+# Consignas
+# Clientes
+# 1) ¿Cuántos clientes existen?
+
+SELECT COUNT(*) FROM Clientes;
+
+# 2) ¿Cuántos clientes hay por ciudad?
+
+SELECT COUNT(*), ciudad FROM Clientes GROUP BY ciudad; 
+
+# FACTURAS
+# 1) ¿Cuál es el total de transporte?
+
+SELECT SUM(Transporte) AS  'Total Transporte' FROM Facturas; 
+
+# 2) ¿Cuál es el total de transporte por EnvioVia (empresa de envío)?
+
+SELECT SUM(Transporte), EnvioVia FROM Facturas GROUP BY EnvioVia; 
+
+# 3) Calcular la cantidad de facturas por cliente. Ordenar descendentemente por cantidad de facturas.
+
+SELECT ClienteID, COUNT(ClienteID) AS CantidadFacturas 
+FROM Facturas 
+GROUP BY ClienteID 
+ORDER BY CantidadFacturas DESC; 
+
+# 4) Obtener el Top 5 de clientes de acuerdo a su cantidad de facturas.
+
+SELECT ClienteID, COUNT(ClienteID) AS CantidadFacturas 
+FROM Facturas 
+GROUP BY ClienteID 
+ORDER BY CantidadFacturas DESC
+LIMIT 5; 
+
+# 5) ¿Cuál es el país de envío menos frecuente de acuerdo a la cantidad de facturas?
+SELECT * FROM Facturas; 
+
+SELECT PaisEnvio, COUNT(PaisEnvio) AS CantidadFacturas 
+FROM Facturas 
+GROUP BY PaisEnvio 
+ORDER BY CantidadFacturas;
+
+# 6) Se quiere otorgar un bono al empleado con más ventas. ¿Qué ID de empleado realizó más operaciones de ventas?
+
+
+
+# Factura detalle
+# 1) ¿Cuál es el producto que aparece en más líneas de la tabla Factura Detalle?
+# 2) ¿Cuál es el total facturado? Considerar que el total facturado es la suma de cantidad por precio unitario.
+# 3) ¿Cuál es el total facturado para los productos ID entre 30 y 50?
+# 4) ¿Cuál es el precio unitario promedio de cada producto?
+# 5) ¿Cuál es el precio unitario máximo?
+
+
+
+#PARTE II
+# 1) Generar un listado de todas las facturas del empleado 'Buchanan'.
+# 2) Generar un listado con todos los campos de las facturas del correo 'SpeedyExpress'.
+# 3) Generar un listado de todas las facturas con el nombre y apellido de los empleados.
+# 4) Mostrar un listado de las facturas de todos los clientes “Owner” y país de envío “USA”.
+# 5) Mostrar todos los campos de las facturas del empleado cuyo apellido sea “Leverling” o que incluyan el producto id = “42”.
+# 6) Mostrar todos los campos de las facturas del empleado cuyo apellido sea “Leverling” y que incluya los producto id = “80” o ”42”.
+# 7) Generar un listado con los cinco mejores clientes, según sus importes de compras total (PrecioUnitario * Cantidad).
+# 8) Generar un listado de facturas, con los campos id, nombre y apellido del cliente, fecha de factura, país de envío, Total, ordenado de manera descendente por fecha de factura y limitado a 10 filas.
